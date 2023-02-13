4000fdaa0fd8b5dc473e1f7b8bafad02735f6fe2
bangAzee22 <124791662+bangAzee22@users.noreply.github.com>
2023-02-13 23:37:04 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -3,12 +3,12 @@ let handler = async (m, { conn, text, usedPrefix }) => {
 
     let img = fs.readFileSync('./thumbnail.jpg');
     let img2 = `https://telegra.ph/file/8f7e2b662ac8dc4f7f562.jpg`
-    return conn.sendButton(m.chat, 'Pm Owner aja \n❏ https://wa.me/628812743180 ', 'Makasih, thanks for you donate...', img2, [["MENU", usedPrefix + "menu"], ["OWNER", usedPrefix + "owner"]], m, {
+    return conn.sendButton(m.chat, 'Pm Owner aja \n❏ https://wa.me/6282169638085 ', 'Makasih, thanks for you donate...', img2, [["MENU", usedPrefix + "menu"], ["OWNER", usedPrefix + "owner"]], m, {
         contextInfo: {
             externalAdReply: {
                 mediaUrl: "https://instagram.com/saep.02",
                 mediaType: "VIDEO",
-                title: 'Donasi To NexEp ',
+                title: 'Donasi To Bang Azee ',
                 body: bottime,
                 previewType: 0,
                 thumbnail: img
